e7b3dc717edfdc13098bd1181c15fd1e1a4a1315
David Caratelli <devish.c@gmail.com>
2016-04-11 13:50:46 -0500

    update to Pi0 filter -- David Caratelli

--- Playground/Pi0Filter.cxx
+++ Playground/Pi0Filter.cxx
@@ -25,22 +25,33 @@ namespace larlite {
 
     int n_pi0 = 0;
 
-    for (auto const& part : part_v){
+    size_t pi0_idx = 0;
+
+    for (size_t i=0; i < part_v.size(); i++){
+      auto const& part = part_v[i];
       //std::cout << "found " << part.PdgCode() << std::endl;
       if ( (part.PdgCode() == 111) and (part.StatusCode() == 1) ){
 	std::cout << "found " << part.PdgCode() << "\tw/ energy : " << part.Trajectory().at(0).E() <<  std::endl;
 	n_pi0 += 1;
-      }
+	pi0_idx = i;
+	  }
     }
-
+    
     if (n_pi0 != _n_pi0s)
       return false;
-  
-    return true;
+    
+    // is the vertex in the TPC?
+    auto const& vtx = part_v.at(pi0_idx).Trajectory().at(0);
+    if ( (vtx.X() > 0) and (vtx.X() < 256) and (vtx.Y() > -116) and (vtx.Y() < 116) and (vtx.Z() > 0) and (vtx.Z() < 1036) ){
+      std::cout << "Vertex @ x : " << vtx.X() << "\t y : " << vtx.Y() << "\t z : " << vtx.Z() << std::endl;
+      return true;
+    }
+    
+    return false;
   }
-
+  
   bool Pi0Filter::finalize() {
-
+    
     return true;
   }
 
