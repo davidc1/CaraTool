f6c7b2be4e68c9eff08c019bc35a268f7bc5826b
david <devish.c@gmail.com>
2017-09-13 19:56:55 -0500

    handclusteirng tools added --DC

---
+++ HandClusterInput/LinkDef.h
@@ -0,0 +1,17 @@
+//
+// cint script to generate libraries
+// Declaire namespace & classes you defined
+// #pragma statement: order matters! Google it ;)
+//
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class larlite::AddClusters+;
+#pragma link C++ class larlite::AddClusters2Pi0+;
+#pragma link C++ class larlite::CheckVertex+;
+//ADD_NEW_CLASS ... do not change this line
+#endif
+
